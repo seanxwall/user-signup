9bffbd0cb8fbdba0a2d405263f06356cc25271af
seanxwall <31492950+seanxwall@users.noreply.github.com>
2017-10-16 23:16:46 -0500

    Finished main.py and templates for final version of the web app

--- main.py
+++ main.py
@@ -1,91 +1,68 @@
-from flask import Flask, request, render_template, redirect, flash
+from flask import Flask, request, render_template, redirect
 #from flask_wtf import form, StringField
 import cgi
 
 app = Flask(__name__)
 
+error_code = False
+
 app.config['DEBUG'] = True 
-
-# shouldn't need two routes to the same location
-# just check what brought you here
-'''
-@app.route("/", methods=['POST'])
-def get_username():
-    username = cgi.escape(request.form['username'])
-    if (len(username)<3) or (len(username)>20):
-        error = "Sorry, but that's not a valid username"
-        return error    
-
-@app.route("/", methods=['POST'])
-def get_password():  
-    password = cgi.escape(request.form['password'])
-    if (len(password)<3) or (len(password)>20):
-        error = "Sorry, but that's not a valid password"
-        return error   
-        
-@app.route("/", methods=['POST'])
-def confirm_password():
-    verify = cgi.escape(request.form['verify'])
-    if verify != password: 
-        error = "Oops, your passwords don't match!"
-        return error
-'''
-
-# Create a form to regiter with
-'''
-class RegisterForm(Form):
-    username = StringField('username')
-'''
 
 
 @app.route("/", methods=['GET','POST'])
 def forms():
+    error_code = False
     
-    if request.method == 'POST':
+    if request.method == 'GET':
+        return render_template('forms.html', error_code=error_code)
+
+    elif request.method == 'POST':
+
         username = cgi.escape(request.form['username'])
         password = cgi.escape(request.form['password'])
         verify = cgi.escape(request.form['verify'])
         email = cgi.escape(request.form['email'])
-        if username == '':
-            render_template('welcome.html')
-        #handle post validate fields
-        redirect('welcome')
-    # first time getting the page render it for them
-    elif request.method == 'GET':
-        render_template('welcome.html')
+       
+        # Validate usernames length
+        user_error = ''
+        if not ((len(username)>3) and (len(username)<20)):
+            error_code = True
+            user_error = "Your username needs to be between 3 and 20 characters"
+            
+        # Validate passwords length
+        password_error = ''
+        if not (((len(password)>3) and (len(password)<20))):
+            error_code = True
+            password_error = "Passwords need to be between 3 and 20 characters"
 
+        verify_error = ''
+        if verify == '':
+            error_code = True
+            verify_error = "Passwords need to be between 3 and 20 characters"
+        # Validate passwords match
+        if verify != password:
+            error_code = True
+            verify_error = "Passwords didn't match"
 
+        # check for @ symbol and dot in string or for white space
+        email_error = ''
+        if email == '':
+            email_error = ''
+        elif '@' not in email and '.' not in email or ' ' in email:
+                error_code = True
+                email_error = "Not a valid email address"
+            
+        # render the page again with errors if error_code is True
+        if error_code == True:
+            return render_template('forms.html', username=username, email=email, error_code=error_code, user_error=user_error, password_error=password_error, verify_error=verify_error, email_error=email_error)
+            return render_template('forms.html')
+        else:
+            return render_template('welcome.html', username=username)
     
-    return render_template('forms.html')
+    return render_template('forms.html', error_code=error_code)
  
-@app.route("/welcome", methods=['POST'])
-def welcome():
-    username = cgi.escape(request.form['username'])
-    password = cgi.escape(request.form['password'])
-    verify = cgi.escape(request.form['verify'])
-    email = cgi.escape(request.form['email'])
-    if ((len(username)>3) and (len(username)<20)) and (((len(password)>3) and (len(password)<20))) and (verify == password) and (' ' not in email) and ('@' in email) and ('.' in email):
-        return render_template('welcome.html', username=username)
 
-'''     
-@app.route("/")
-def index():
-    user_error = request.args.get("error")
-    return render_template('forms.html', error=user_error and cgi.escape(user_error, quote=True))
-'''
 
-def email_check(email):
-    if ' ' in email: 
-        error = "Sorry, but that's not a valid email address"
-        return error    
-    if '@' not in email: 
-        error = "Sorry, but that's not a valid email address"
-        return error      
-    if '.' not in email: 
-        error = "Sorry, but that's not a valid email address"
-        return error
+
 
 app.run()
-
-
-
